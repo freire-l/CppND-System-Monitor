22933ec479a99509da190377f157f44dc7cd1d3b
freire-l <85429031+freire-l@users.noreply.github.com>
2022-09-29 10:57:04 -0500

    Update comments to process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -26,22 +26,22 @@ Process::Process(int my_id) : pid_(my_id) {
     cpu_util_ = Cpu_Util_calc();
 }
 
-// TODO: Return this process's ID
+// Return this process's ID
 int Process::Pid() const { return pid_; }
 
-// TODO: Return this process's CPU utilization
+// Return this process's CPU utilization
 float Process::CpuUtilization() const { return cpu_util_;}
 
-// TODO: Return the command that generated this process
+// Return the command that generated this process
 string Process::Command() const { return command_;}
 
-// TODO: Return this process's memory utilization
+// Return this process's memory utilization
 string Process::Ram() const { return to_string(ram_); }
 
-// TODO: Return the user (name) that generated this process
+// Return the user (name) that generated this process
 string Process::User() const { return user_; }
 
-// TODO: Return the age of this process (in seconds)
+// Return the age of this process (in seconds)
 long int Process::UpTime() const { return up_time_; }
 
 //Calculate cpu utiization
@@ -51,8 +51,8 @@ float Process::Cpu_Util_calc(){
     long start_time = LinuxParser::UpTime(pid_)/sysconf(_SC_CLK_TCK); //start time of the process
     return ((float) active / (((float)sys_uptime)-((float)start_time))); 
 }
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
+// Overload the "less than" comparison operator for Process objects
+
 bool Process::operator<(Process const& a) const { 
 
     return a.cpu_util_ < this->cpu_util_;
