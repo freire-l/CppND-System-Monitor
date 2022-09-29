d81ac5fc1c4318d659127ac6c49e35d2d99147ea
freire-l <85429031+freire-l@users.noreply.github.com>
2022-09-29 10:59:50 -0500

    Update comments to system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -21,7 +21,7 @@ using std::cout;
 
 //github test
 
-// TODO: Return the system's CPU
+// Return the system's CPU
 //First we call CpuUtilization from LinuxParses, which reads and parses the infro from /proc/stat and saves it into a vector
 //We use that vector to feed the private members of cpu, that will be used to calculate the utilization
 Processor& System::Cpu() { 
@@ -30,7 +30,7 @@ Processor& System::Cpu() {
     return cpu_; 
 }
 
-// TODO: Return a container composed of the system's processes
+// Return a container composed of the system's processes
 vector<Process>& System::Processes() { 
     processes_.clear();
     vector<int>  process_list = LinuxParser::Pids();
@@ -43,22 +43,22 @@ vector<Process>& System::Processes() {
     return processes_; 
 }
 
-// TODO: Return the system's kernel identifier (string)
+// Return the system's kernel identifier (string)
 std::string System::Kernel() const{ return LinuxParser::Kernel(); }
 
-// TODO: Return the system's memory utilization
+// Return the system's memory utilization
 float System::MemoryUtilization() const { return LinuxParser::MemoryUtilization(); }
 
-// TODO: Return the operating system name
+// Return the operating system name
 std::string System::OperatingSystem() const { return LinuxParser::OperatingSystem(); }
 
-// TODO: Return the number of processes actively running on the system
+// Return the number of processes actively running on the system
 int System::RunningProcesses() const { return LinuxParser::RunningProcesses(); }
 
-// TODO: Return the total number of processes on the system
+// Return the total number of processes on the system
 int System::TotalProcesses() const { return LinuxParser::TotalProcesses(); }
 
-// TODO: Return the number of seconds since the system started running
+// Return the number of seconds since the system started running
 //long int System::UpTime() { Format::ElapsedTime(LinuxParser::UpTime()); } 
 long int System::UpTime() const { return LinuxParser::UpTime(); }
 
